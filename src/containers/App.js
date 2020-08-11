da14e8172cfb241bbc502a5274a9e62396d8d006
Michelle Williams <michelle.williams@dominos.com>
2020-08-11 16:34:13 -0400

    get ajax calls working

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,33 @@ import Header from "../components/Header/Header";
 import axios from "axios";
 
 const App = () => {
+  let [weatherData, setWeatherData] = useState(null);
+
+  const setCurrentCity = (position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=imperial
+          `
+      )
+      .then((response) => {
+        if (response.status > 400) {
+          throw response;
+        }
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(setCurrentCity);
+    }
+  }, []);
+
   return (
     <div className="App">
       <Header />
